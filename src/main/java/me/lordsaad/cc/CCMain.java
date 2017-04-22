5500dd2be33e22269a4b6de0d080af67eff46d47
Saad <saadodi44@gmail.com>
2017-04-22 12:59:32 +0300

    update version number to 1.1 & update liblib to 3.1.3

--- src/main/java/me/lordsaad/cc/CCMain.java
+++ src/main/java/me/lordsaad/cc/CCMain.java
@@ -23,7 +23,7 @@ public class CCMain {
 
 	public static final String MOD_ID = "cc";
 	public static final String MOD_NAME = "Cranes & Construction";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	public static final String CLIENT = "me.lordsaad.cc.client.ClientProxy";
 	public static final String SERVER = "me.lordsaad.cc.common.CommonProxy";
